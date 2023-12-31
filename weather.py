5c486e4a533abb3cd3d5bd7b3e9a38994d982c5d
Manu <117709053+gravityboots@users.noreply.github.com>
2024-01-01 00:42:27 +0530

    big update bois
    weather app files without working forecast or responsive ui but it works lets go

---
+++ weather.py
@@ -0,0 +1,48 @@
+import requests
+import json
+from datetime import datetime
+
+
+def localTime(utc_with_tz):
+    local_time = datetime.fromtimestamp(utc_with_tz)
+    return local_time
+
+
+def getWeatherOld(place):
+    response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={place}&appid=460de012b0fd52774245f52ed38c4888')
+    response = response.json()
+    
+    if response['cod']==200:
+        temperature = round((response['main']['temp'] - 273.15),2)
+        feels_like_temp = round((response['main']['feels_like'] - 273.15),2)
+        pressure = response['main']['pressure']
+        humidity = response['main']['humidity']
+        wind = response['wind']['speed']
+        timezone = response['timezone']
+        sunrise = localTime(response['sys']['sunrise'] + timezone)
+        sunset = localTime(response['sys']['sunset'] + timezone)
+        cloudy = response['clouds']['all']
+        description = response['weather'][0]['description']
+
+        weather = place, temperature, feels_like_temp, pressure ,humidity, sunrise, sunset, cloudy, wind, description
+
+    else:
+        weather = response['cod'], response['message']
+    
+    return weather
+
+
+def getWeather(place):
+    response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={place}&appid=460de012b0fd52774245f52ed38c4888')
+    response = response.json()
+    return response
+
+
+def getForecast(place):
+    response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid=460de012b0fd52774245f52ed38c4888')
+    response = response.json()    
+    return response
+
+
+def getWeatherMap(x, y, z, layer):
+    pass
